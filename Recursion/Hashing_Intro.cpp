a6335b388d17f0543380dea9ca78280bb2e4aca5
dhruv vyas <dhruvvyas3005@gmail.com>
2026-01-10 21:20:05 +0530

    Hashing programs commited

---
+++ Recursion/Hashing_Intro.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	//now we are implementing the hashing for the ver first time
+	
+	int n;
+	cin>>n;
+	
+	int arr[n];
+	
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	
+	//precompute phase;
+	
+	int hash[13]={0};
+	
+	for(int i=0;i<n;i++){
+		hash[arr[i]]+=1;
+	}
+	
+	int q;
+	cin>>q;
+	
+	while(q--){
+		int number;
+		
+		cin>>number;
+		
+		//fetching
+		
+		cout<<hash[number]<<endl;
+		
+	}
+	
+	return 0;
+}
